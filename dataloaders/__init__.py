164a748f48f2326fb8f177f221224f09bd8481f8
jb <jeanbenoit.delbrouck@gmail.com>
2021-01-14 13:25:43 +0100

    ok

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,4 +1,3 @@
 from .MimicDataset.MimicDataset import MimicDataset
-from .MimicDataset.OldMimicDataset import OldMimicDataset
 from .MimicDataset.VectorMimicDataset import VectorMimicDataset
 from .BaseDataset import BaseDataset
